8811618df5284fc21d401a7a5fcb49ac5cfb4090
Vasil Remeniuk <vasil.remeniuk@gmail.com>
2014-05-03 18:49:30 +0300

    added travis config

---
+++ src/test/scala/com/bokland/rubbercube/measure/kpi/mobile/MobileMeasuresSpec.scala
@@ -0,0 +1,10 @@
+package com.bokland.rubbercube.measure.kpi.mobile
+
+import org.specs2.mutable.Specification
+
+/**
+ * Created by remeniuk on 5/3/14.
+ */
+class MobileMeasuresSpec extends Specification {
+
+}
